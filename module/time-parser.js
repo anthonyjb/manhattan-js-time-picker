69177b0c4d97e5aa16c9b6dc33558f8c0fe4d020
Anthony <ant@getme.co.uk>
2020-03-21 14:07:55 +0000

    Work towards documenting parsers

--- module/time-parser.js
+++ module/time-parser.js
@@ -1,18 +1,4 @@
 
-// 10:00:00
-// 10:00
-// 10am
-// 10:00am
-// 10.00
-// 10.00am
-// 10.00a.m
-// 10.00a.m.
-// 21h45
-// 21h45min
-// noon
-// 12 noon
-// midnight
-// 12 midnight
 
 
 /**
@@ -45,6 +31,9 @@ export class TimeParser {
 
 }
 
+
+// -- Formatters --
+
 /**
  * A set of functions that can ouput 24h times (string) in various string
  * formats.
@@ -52,9 +41,81 @@ export class TimeParser {
 TimeParser.formatter = {
 
     /**
-     * Format time as `12h`, e.g 11:00pm
+     * Format time as 12h with minutes, e.g 11:00pm.
      */
+    '12hm': (inst, time) => {
+        return '@@'
+    },
 
+    /**
+     * Format time as 12h with minutes and seconds, e.g 11:00:00pm.
+     */
+    '12hms': (inst, time) => {
+        return '@@'
+    },
 
+    /**
+     * Format time as 24h with minutes, e.g 13:00.
+     */
+    '24hm': (inst, time) => {
+        return '@@'
+    },
+
+    /**
+     * Format time as 24h with minutes and seconds, e.g 13:00:00.
+     */
+    '24hms': (inst, time) => {
+        return '@@'
+    }
 }
 
+
+// -- Parsers --
+
+/**
+ * A set of functions that can parse various different time formats.
+ */
+TimeParser.parsers = {
+
+    /**
+     * Return a 24h time (string) from a string using the 24h format
+     * (`HH:MM:SS`), e.g:
+     *
+     * - 1
+     * - 1:00
+     * - 1:00:00
+     * - 01
+     * - 13
+     * - 13:00
+     * - 13:00:00
+     * - 13.00
+     * - 13.00.00
+     * - 13h
+     * - 13h00m
+     * - 13h00m00s
+     *
+     */
+    '24h': (inst, s) => {
+
+    },
+
+    /**
+     * Return a 24h time (string) from a string using the 12h format, e.g:
+     *
+     * - 1 am
+     * - 1 a.m
+     * - 1 a.m.
+     * - 1:00 am
+     * - 1:00:00 am
+     * - noon
+     * - 12 noon
+     * - midnight
+     * - 12 midnight
+     *
+     * the separator can be a `:` or '.', minutes and seconds are optional.
+     */
+    '12h': (inst, s) => {
+
+    }
+
+}
