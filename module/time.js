633e8c5ae7258170dcf78d865694bb1822082c51
Anthony Blackshaw <ant@getme.co.uk>
2020-03-21 17:32:43 +0000

    Added spec for time class

--- module/time.js
+++ module/time.js
@@ -4,11 +4,14 @@
  */
 export class Time {
 
-    constructor(hours, minutes, seconds) {
+    constructor(hour, minute, second) {
+
+        // Initialize the time
         this._hour = 0
         this._minute = 0
         this._second = 0
 
+        // Set the time
         this.hour = hour || 0
         this.minute = minute || 0
         this.second = second || 0
@@ -39,7 +42,7 @@ export class Time {
     }
 
     get second() {
-        return this.second
+        return this._second
     }
 
     set second(second) {
@@ -52,31 +55,13 @@ export class Time {
     // Methods
 
     /**
-     * Add the given number of hours to the time.
-     */
-    addHours(hours) {
-        this.addSeconds(hours * 3600)
-    }
-
-    /**
-     * Add the given number of minutes to the time.
-     */
-    addMinutes(minutes) {
-        this.addSeconds(minutes * 60)
-    }
-
-    /**
-     * Add the given number of seconds to the time.
-     */
-    addSeconds(seconds) {
-
-    }
-
-    /**
      * Return the time as a string
      */
     toString() {
-
+        const hourStr = this.hour.toString().padStart(2, '0')
+        const minuteStr = this.minute.toString().padStart(2, '0')
+        const secondStr = this.second.toString().padStart(2, '0')
+        return `${hourStr}:${minuteStr}:${secondStr}`
     }
 
 }
---
+++ spec/time.spec.js
@@ -0,0 +1,108 @@
+
+import * as chai from 'chai'
+import * as sinon from 'sinon'
+
+import * as setup from './setup.js'
+import {Time} from '../module/time.js'
+
+chai.should()
+chai.use(require('sinon-chai'))
+
+
+// Tests
+
+describe('Time', () => {
+
+    describe('constructor', () => {
+        it('should generate a new `Time` instance', () => {
+            const time = new Time()
+            time.should.be.an.instanceof(Time)
+        })
+
+        it('should allow the initial time to be set', () => {
+            const time = new Time(11, 12, 13)
+            time.toString().should.equal('11:12:13')
+        })
+
+    })
+
+    describe('getters & setters', () => {
+        let time = null
+
+        beforeEach(() => {
+            time = new Time(11, 12, 13)
+        })
+
+        describe('hour', () => {
+
+            it('should get the hour for the time', () => {
+                time.hour.should.equal(11)
+            })
+
+            it('should set the hour for the time', () => {
+                time.hour = 14
+                time.hour.should.equal(14)
+            })
+
+            it('should throw an error an invalid hour is specified', () => {
+                chai
+                    .expect(() => { time.hour = 25 })
+                    .to
+                    .throw('Not a valid hour')
+            })
+        })
+
+        describe('minute', () => {
+
+            it('should get the minute for the time', () => {
+                time.minute.should.equal(12)
+            })
+
+            it('should set the minute for the time', () => {
+                time.minute = 20
+                time.minute.should.equal(20)
+            })
+
+            it('should throw an error an invalid minute is specified', () => {
+                chai
+                    .expect(() => { time.minute = 80 })
+                    .to
+                    .throw('Not a valid minute')
+            })
+        })
+
+        describe('second', () => {
+
+            it('should get the second for the time', () => {
+                time.second.should.equal(13)
+            })
+
+            it('should set the second for the time', () => {
+                time.second = 20
+                time.second.should.equal(20)
+            })
+
+            it('should throw an error an invalid second is specified', () => {
+                chai
+                    .expect(() => { time.second = 80 })
+                    .to
+                    .throw('Not a valid second')
+            })
+        })
+    })
+
+    describe('public methods', () => {
+        let time = null
+
+        beforeEach(() => {
+            time = new Time(11, 12, 13)
+        })
+
+        describe('format', () => {
+            it('should return a string representing the time', () => {
+                time.toString().should.equal('11:12:13')
+            })
+        })
+
+    })
+})
